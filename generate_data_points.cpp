8fcdc65dc82d567c89274cf8b7d7b7d0d3371c1d
rrk101 <111215860+rrk101@users.noreply.github.com>
2022-08-14 11:27:43 +0530

    Update generate_data_points.cpp

--- generate_data_points.cpp
+++ generate_data_points.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 
 const char *output_file_name = "vector_data.txt";
-const char *input_file_name = "vector_data.txt";
+
 const int N = 10000000;
 
 const float range_from  = -100.0f;
@@ -14,26 +14,9 @@ uniform_real_distribution<float>  distr(range_from, range_to);
 
 int main()
 {
-    //freopen(output_file_name,"w",stdout);
-    //freopen(input_file_name,"r",stdin);
-    //int n;
-    //cin>>n;
-    //n = N;
-    //n = 10;
-    //float data[n][2];
-    //vector<float> query(100,0.0f);
-    // vector<vector<float>> data(n,vector<float>(100));
-    // for(auto &i:data)
-    // {
-    //     for(auto &j:i) j = distr(rng);
-    // }
-    //
-    // for(auto &i:data)
-    // {
-    //
-    // }
+
     {
-        FILE *file  = fopen("new_vector_data.dat","wb");
+        FILE *file  = fopen(output_file_name,"wb");
             for(int i=0;i<N;i++)
             {
                 for(int j=0;j<100;j++)
